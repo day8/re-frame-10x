6bf1648187d251aae0f8d500ceec61668e37c1d4
Daniel Compton <desk@danielcompton.net>
2016-11-06 16:50:51 +1300

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject day8.re-frame/trace "0.0.1-SNAPSHOT"
+  :description "Tracing and developer tools for re-frame apps"
+  :url         "https://github.com/Day8/re-frame-trace"
+  :license     {:name "MIT"}
+  :dependencies [[org.clojure/clojure        "1.8.0"]
+                 [org.clojure/clojurescript  "1.9.227"]
+                 [reagent                    "0.6.0"]]
+  )
