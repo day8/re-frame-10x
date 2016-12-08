8504482965257f9409e2538f601299e8c83b37ef
Daniel Compton <desk@danielcompton.net>
2016-12-09 10:41:34 +1300

    Update to 0.9.0 reframe alpha

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure        "1.8.0"]
                  [org.clojure/clojurescript  "1.9.227"]
                  [reagent                    "0.6.0"]
-                 [re-frame                   "0.8.1-SNAPSHOT"]
+                 [re-frame                   "0.9.0-alpha1"]
                  [cljsjs/d3 "4.2.2-0"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
