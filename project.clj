a1234cb3821ef17c9b8ecb63780e3606f1f16147
Daniel Compton <desk@danielcompton.net>
2017-04-13 12:03:58 +1200

    Version 0.0.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day8.re-frame/abra "0.0.8"
+(defproject day8.re-frame/abra "0.0.9-SNAPSHOT"
   :description "Tracing and developer tools for re-frame apps"
   :url         "https://github.com/Day8/re-frame-trace"
   :license     {:name "MIT"}
