6bee5c3a635eaf99ac0a051bd801f419a711b285
Daniel Compton <desk@danielcompton.net>
2017-08-31 16:23:46 +1200

    Use the correct location for getting CSS

--- src/day8/re_frame/trace/styles.cljs
+++ src/day8/re_frame/trace/styles.cljs
@@ -1,4 +1,4 @@
 (ns day8.re-frame.trace.styles
   (:require-macros [day8.re-frame.trace.macros :as macros]))
 
-(def panel-styles (macros/slurp-macro "day8/re_frame/css/main.css"))
+(def panel-styles (macros/slurp-macro "day8/re_frame/trace/main.css"))
