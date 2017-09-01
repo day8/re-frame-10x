fd25b4413224521a689bc5d02c82acc978f2ff38
chris (daiyi) <daiyitastic@gmail.com>
2017-09-01 16:04:20 +0200

    Add basic data structure views and styles

--- src/day8/re_frame/trace.cljs
+++ src/day8/re_frame/trace.cljs
@@ -394,7 +394,6 @@
                                           :subvis [subvis/render-subvis traces
                                                     [:div.panel-content-scrollable]]
                                           :app-state [app-state/tab @db/app-db])]]]))})))
-
 (defn panel-div []
   (let [id    "--re-frame-trace--"
         panel (.getElementById js/document id)]
