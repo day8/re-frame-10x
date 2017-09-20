e26c5ff255cf523edf06ad5cacc706e54bb35c9c
chris (daiyi) <daiyitastic@gmail.com>
2017-09-20 09:03:45 +0200

    App state strings and integers as editable fields

--- src/day8/re_frame/trace/app_state.cljs
+++ src/day8/re_frame/trace/app_state.cljs
@@ -11,12 +11,41 @@
        (reduce (fn [acc [property value]]
                  (assoc acc (keyword property) value)) {})))
 
+(defn data-input [{:keys [title on-save on-change on-stop]}]
+  (let [val  (r/atom title)
+        save #(let [v (-> @val str str/trim)]
+                (when (pos? (count v))
+                  (on-save v)))]
+    (fn []
+      [:input {:type        (cond (integer? "number")
+                                  (string?  "text"))
+               :value       @val
+               :size        (if (zero? (count (str @val)))
+                              1
+                              (count (str @val)))
+               :on-change   #(do (reset! val (-> % .-target .-value))
+                                 (on-change %))
+               :on-key-down #(case (.-which %)
+                               13 (save)
+                               nil)}])))
+
+(defn editable
+  [raw]
+  (let [val (r/atom raw)]
+    [:span {:class (str/join " " ["data-structure-editable"
+                                  (when (string? raw) "string")])}
+      [data-input {:title     raw
+                   :on-save   #(js/console.log %)
+                   :on-change #(reset! val (.. % -target -value))}]]))
+
+
 (defn str->hiccup
   [string]
-  (cond (= string "span")   :span
-        (= string "style")  :style
-        (= string ", ")     " "
-        :else               string))
+  (cond (= string "span")              :span
+        (= string "style")             :style
+        (= string ", ")                " "
+        (str/starts-with? string "\"") [editable (str/replace string "\"" "")]
+        :else                          string))
 
 (declare jsonml->hiccup)
 
@@ -45,8 +74,8 @@
          (= "object" (first jsonml))) [data-structure jsonml]
     (array? jsonml)                   (mapv jsonml->hiccup jsonml)
     (object? jsonml)                  {:style (string->css (js->clj jsonml))}
-    (or (string? jsonml)
-        (integer? jsonml))            (str->hiccup jsonml)))
+    (string? jsonml)                  (str->hiccup jsonml)
+    (integer? jsonml)                 [editable jsonml]))
 
 (defn tab [data]
   [:div {:style {:flex "1 0 auto" :width "100%" :height "100%" :display "flex" :flex-direction "column"}}
