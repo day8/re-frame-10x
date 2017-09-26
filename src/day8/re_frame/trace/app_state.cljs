f4ce962ae34c9bbdea618c649cc1937baf389734
chris (daiyi) <daiyitastic@gmail.com>
2017-09-26 22:50:41 +0200

    Add & remove sub-paths into app-state

--- src/day8/re_frame/trace/app_state.cljs
+++ src/day8/re_frame/trace/app_state.cljs
@@ -1,7 +1,31 @@
 (ns day8.re-frame.trace.app-state
   (:require [reagent.core :as r]
             [clojure.string :as str]
-            [devtools.formatters.core :as cljs-devtools]))
+            [devtools.formatters.core :as cljs-devtools]
+            [day8.re-frame.trace.localstorage :as localstorage]))
+
+;; TODO move search-input into components ns
+
+(defn search-input [{:keys [title placeholder on-save on-change on-stop]}]
+  (let [val  (r/atom title)
+        save #(let [v (-> @val str str/trim)]
+                (when (pos? (count v))
+                  (on-save v)))]
+    (fn []
+      [:input {:type        "text"
+               :value       @val
+               :auto-focus  true
+               :placeholder placeholder
+               :size        (if (> 20 (count (str @val)))
+                              25
+                              (count (str @val)))
+               :on-change   #(do (reset! val (-> % .-target .-value))
+                                 (on-change %))
+               :on-key-down #(case (.-which %)
+                               13 (do
+                                    (save)
+                                    (reset! val ""))
+                               nil)}])))
 
 (defn string->css [css-string]
   "This function converts jsonml css-strings to valid css maps for hiccup.
@@ -49,7 +73,50 @@
         (= jsonml ", ")             " "
         :else jsonml))))
 
-(defn render-state  [data]
-  [:div {:style {:flex "1 0 auto" :width "100%" :height "100%" :display "flex" :flex-direction "column"}}
-    [:div.panel-content-scrollable
-     (jsonml->hiccup (cljs-devtools/header-api-call @data))]])
+(defn render-state [data]
+  (let [subtree-input  (r/atom "")
+        subtree-paths  (r/atom (localstorage/get "subtree-paths" #{}))
+        input-error    (r/atom false)]
+    (add-watch subtree-paths
+               :update-localstorage
+               (fn [_ _ _ new-state]
+                 (localstorage/save! "subtree-paths" new-state)))
+    (fn []
+      [:div {:style {:flex "1 0 auto" :width "100%" :height "100%" :display "flex" :flex-direction "column"}}
+        [:div.panel-content-scrollable {:style {:margin 10}}
+          [:div.filter-control-input
+            [search-input {:placeholder ":path :into :app-state"
+                           :on-save (fn [path]
+                                      (if false ;; TODO check if path exists in app-state
+                                        (reset! input-error true)
+                                        (do
+                                          ; (reset! input-error false)
+                                          ;; TODO check if input already wrapped in braces
+                                          (swap! subtree-paths conj (cljs.reader/read-string (str "[" path "]"))))))
+                           :on-change #(reset! subtree-input (.. % -target -value))}]]
+            ; (if @input-error
+            ;   [:div.input-error {:style {:color "red" :margin-top 5}}
+            ;    "Please enter a valid path."])]]
+
+          [:div.subtrees {:style {:margin "20px 0"}}
+            (doall
+              (map (fn [path]
+                     ^{:key path}
+                     [:div.subtree-wrapper {:style {:margin "10px 0"}}
+                       [:div.subtree
+                         [:button.subtree-button {:on-click #(swap! subtree-paths disj path)}
+                           [:span.subtree-button-string
+                             (str path)]]
+                         [:div {:style {:margin-top 10
+                                        :margin-left 10
+                                        :padding-bottom 10}}
+                           (if-let [jsonml (cljs-devtools/header-api-call (get-in @data path))]
+                             (jsonml->hiccup jsonml)
+                             (get-in @data path))]]])
+                @subtree-paths))]
+
+          [:div [:h1 {:style {:font-size 20
+                              :margin-top 30
+                              :display "block"}}
+                  "app-state"]]
+          [:div {:style {:margin-top 10}} (jsonml->hiccup (cljs-devtools/header-api-call @data))]]])))
