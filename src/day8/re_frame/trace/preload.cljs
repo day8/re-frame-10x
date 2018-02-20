acea185cc2d2f7b3d8f651953515a15500de02e5
Daniel Compton <desk@danielcompton.net>
2018-02-20 16:26:31 +1300

    Add deprecated preload namespace back in

---
+++ src/day8/re_frame/trace/preload.cljs
@@ -0,0 +1,5 @@
+(ns day8.re-frame.trace.preload
+  (:require [day8.re-frame-10x.preload]))
+
+(js/console.warn
+  "The preload at day8.re-frame.trace.preload is deprecated. Update your `:preloads` to use `day8.re-frame-10x.preload` instead. See https://github.com/Day8/re-frame-10x#installation for more details.")
