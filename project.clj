dfd4097ca16ecee0578ca56718895f1234d3700d
Daniel Compton <desk@danielcompton.net>
2018-03-24 21:22:27 +1300

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day8.re-frame/re-frame-10x "0.3.0"
+(defproject day8.re-frame/re-frame-10x "0.3.1-SNAPSHOT"
   :description "Become 10x more productive when developing and debugging re-frame applications."
   :url "https://github.com/Day8/re-frame-10x"
   :license {:name "MIT"}
