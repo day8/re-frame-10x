42fdafa9b699556bf1b4d42f2b973832a731baab
Daniel Compton <desk@danielcompton.net>
2018-03-26 15:40:59 +1300

    Roll back to react-highlight 1.0.5

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [cljsjs/react-flip-move "2.9.17-0"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.8" :exclusions [rhino/js]]
                  [zprint "0.4.7"]
-                 [cljsjs/react-highlight "1.0.7-0"]
+                 [cljsjs/react-highlight "1.0.5-0"]
                  ;[expound "0.4.0"]
                  ]
   :plugins [[thomasa/mranderson "0.4.7"]
