217a7657448f0be4f0cd9d207a5174d435cb2b86
Gregg8 <gregg.ramsey@day8.com.au>
2018-03-26 16:35:09 +1100

    Removed redundant panel in app-db tab

--- src/day8/re_frame_10x/view/app_db.cljs
+++ src/day8/re_frame_10x/view/app_db.cljs
@@ -13,8 +13,6 @@
                    [day8.re-frame-10x.utils.re-com :refer [handler-fn]]))
 
 (def delete (macros/slurp-macro "day8/re_frame_10x/images/delete.svg"))
-(def reload (macros/slurp-macro "day8/re_frame_10x/images/reload.svg"))
-(def reload-disabled (macros/slurp-macro "day8/re_frame_10x/images/reload-disabled.svg"))
 (def snapshot (macros/slurp-macro "day8/re_frame_10x/images/snapshot.svg"))
 (def snapshot-ready (macros/slurp-macro "day8/re_frame_10x/images/snapshot-ready.svg"))
 (def round-arrow (macros/slurp-macro "day8/re_frame_10x/images/round-arrow.svg"))
@@ -91,48 +89,15 @@
 
 
 (defn panel-header []
-  (let [app-db-after  (rf/subscribe [:app-db/current-epoch-app-db-after])
-        app-db-before (rf/subscribe [:app-db/current-epoch-app-db-before])]
-    [rc/h-box
-     :justify :between
-     :align :center
-     :margin (css-join common/gs-19s "0px")
-     :style {:flex-flow "row wrap"}
-     :children [[rc/button
+  [rc/h-box
+   :align      :center
+   :style      {:margin-top common/gs-19s}
+   :children   [[rc/button
                  :class "bm-muted-button app-db-panel-button"
                  :label [rc/v-box
                          :align :center
                          :children ["+ path inspector"]]
-                 :on-click #(rf/dispatch [:app-db/create-path])]
-                [rc/h-box
-                 :class "app-db-panel"
-                 :align :center
-                 :gap common/gs-7s
-                 :height "48px"
-                 :children [[rc/label :label "reset app-db to:"]
-                            [rc/button
-                             :class "bm-muted-button app-db-panel-button"
-                             :label [rc/v-box
-                                     :align :center
-                                     :children ["initial epoch state"]]
-                             :on-click #(rf/dispatch [:snapshot/load-snapshot @app-db-before])]
-                            [rc/v-box
-                             :width common/gs-81s
-                             :align :center
-                             :children [[rc/label
-                                         :style {:font-size "9px"}
-                                         :label "EVENT"]
-                                        [:img {:src (str "data:image/svg+xml;utf8," arrow-right)}]
-                                        [rc/label
-                                         :style {:font-size  "9px"
-                                                 :margin-top "-1px"}
-                                         :label "PROCESSING"]]]
-                            [rc/button
-                             :class "bm-muted-button app-db-panel-button"
-                             :label [rc/v-box
-                                     :align :center
-                                     :children ["end epoch state"]]
-                             :on-click #(rf/dispatch [:snapshot/load-snapshot @app-db-after])]]]]]))
+                 :on-click #(rf/dispatch [:app-db/create-path])]]])
 
 
 (defn pod-header-section
