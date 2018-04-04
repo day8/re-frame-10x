9b8a54cf361b6d63cc6647e999cab5435f4b77ac
Daniel Compton <desk@danielcompton.net>
2018-04-05 08:55:59 +1200

    Update to official cljsjs version of react-highlight

--- project.clj
+++ project.clj
@@ -10,8 +10,7 @@
                  [cljsjs/react-flip-move "2.9.17-0"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.8" :exclusions [rhino/js]]
                  [zprint "0.4.7"]
-                 ;; Waiting for https://github.com/cljsjs/packages/pull/1558
-                 [org.clojars.danielcompton/react-highlight "1.0.7-1" :exclusions [cljsjs/react]]
+                 [cljsjs/react-highlight "1.0.7-1" :exclusions [cljsjs/react]]
                  ;[expound "0.4.0"]
                  ]
   :plugins [[thomasa/mranderson "0.4.7"]
