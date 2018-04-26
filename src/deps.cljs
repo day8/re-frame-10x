b2a34d7e8189fa0fac41e7bfbbaa76ef271ed0da
Juho Teperi <juho.teperi@iki.fi>
2018-04-26 17:38:51 +0300

    Add deps.cljs for Shadow-CLJS and Cljs :install-deps

---
+++ src/deps.cljs
@@ -0,0 +1,6 @@
+{:npm-deps {"react" "16.3.0"
+            "react-dom" "16.3.0"
+            "create-react-class" "15.6.2"
+            "react-flip-move" "3.0.1"
+            "highlight.js" "9.12.0"
+            "react-highlight.js" "1.0.7"}}
