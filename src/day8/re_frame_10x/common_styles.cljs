c3f05b73a04a9d2fe686f9ef1a1b333816de550f
Leon Mergen <leon@solatis.com>
2018-12-14 10:07:03 +0100

    Fixes Firefox rendering issue

--- src/day8/re_frame_10x/common_styles.cljs
+++ src/day8/re_frame_10x/common_styles.cljs
@@ -205,7 +205,8 @@
 
    ;; Default text color overrides
    [:body {:color default-text-color}]
-   [:.form-control {:color default-text-color}]
+   [:.form-control {:color default-text-color
+                    :width "100%"}]
    [:.btn-default {:color default-text-color}]
    [:.raptor-editable-block {:color default-text-color}]
 
