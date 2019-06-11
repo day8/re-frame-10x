56e7279a3491924fb9d0019731339197fcfb66e6
Isaac Johnston <isaac@superstruct.nz>
2019-06-11 16:30:58 +0300

    Upgrade dependencies in deps.cljs

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,6 @@
-{:npm-deps {"react" "16.3.0"
-            "react-dom" "16.3.0"
-            "create-react-class" "15.6.2"
-            "react-flip-move" "3.0.1"
-            "highlight.js" "9.12.0"
+{:npm-deps {"react" "16.8.6"
+            "react-dom" "16.8.6"
+            "create-react-class" "15.6.3"
+            "react-flip-move" "3.0.3"
+            "highlight.js" "9.15.8"
             "react-highlight.js" "1.0.7"}}
