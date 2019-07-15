3ff13717d4a8c632e995097691ac2e8fca1d157f
Isaac Johnston <isaac@superstruct.nz>
2019-07-15 10:39:29 +0800

    Upgrade re-frame to 0.10.8

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [reagent "0.8.1" :scope "provided"]
-                 [re-frame "0.10.7" :scope "provided"]
+                 [re-frame "0.10.8" :scope "provided"]
                  [binaryage/devtools "0.9.10"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.8" :exclusions [rhino/js]]
                  [zprint "0.4.16"]
