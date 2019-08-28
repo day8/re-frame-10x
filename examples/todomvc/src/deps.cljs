dd3b7cf077df682dd364dc43d7b45f868040a027
Isaac Johnston <isaac@superstruct.nz>
2019-08-28 15:25:05 +0800

    Upgrade shadow-cljs to 2.8.52 in todomvc example

---
+++ examples/todomvc/src/deps.cljs
@@ -0,0 +1,5 @@
+{:npm-dev-deps {"shadow-cljs"           "2.8.52"
+                "karma"                 "4.3.0"
+                "karma-chrome-launcher" "3.1.0"
+                "karma-cljs-test"       "0.1.0"
+                "karma-junit-reporter"  "1.2.0"}}
