5fc598d48fa3e5c6193dd94c90b73e64023e6f11
Isaac Johnston <isaac@superstruct.nz>
2019-09-09 11:58:37 +0800

    Reformat src/deps.cljs

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,3 +1,3 @@
-{:npm-deps {"react-flip-move" "3.0.3"
-            "highlight.js" "9.15.8"
+{:npm-deps {"react-flip-move"    "3.0.3"
+            "highlight.js"       "9.15.8"
             "react-highlight.js" "1.0.7"}}
