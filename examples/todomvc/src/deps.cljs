b523563cab5d866788f46f1a7756540de52a9318
Isaac Johnston <isaac@superstruct.nz>
2019-11-01 23:22:03 +0800

    Upgrade todomvc example karma-junit-reporter to 2.0.1

--- examples/todomvc/src/deps.cljs
+++ examples/todomvc/src/deps.cljs
@@ -2,4 +2,4 @@
                 "karma"                 "4.4.1"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
-                "karma-junit-reporter"  "1.2.0"}}
+                "karma-junit-reporter"  "2.0.1"}}
