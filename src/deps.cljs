36806422867e9caf1e05e4c946d4432d4c6a8066
Benjamin Albrecht <git@albrecht.io>
2020-02-13 14:00:28 +0100

    fix shadow-cljs warning for using newer react deps

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,4 @@
-{:npm-deps {"react"              "16.9.0"
-            "react-dom"          "16.9.0"
+{:npm-deps {"react"              "^16.9.0"
+            "react-dom"          "^16.9.0"
             "highlight.js"       "9.15.10"
             "react-highlight.js" "1.0.7"}}
