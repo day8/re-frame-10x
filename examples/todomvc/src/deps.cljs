3b397e6248ce872111a9417bbee0583a15501dea
Isaac Johnston <isaac@superstruct.nz>
2020-04-27 15:21:53 +1200

    Upgrade karma to 5.0.2

--- examples/todomvc/src/deps.cljs
+++ examples/todomvc/src/deps.cljs
@@ -1,5 +1,5 @@
 {:npm-dev-deps {"shadow-cljs"           "2.8.104"
-                "karma"                 "4.4.1"
+                "karma"                 "5.0.2"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"}}
