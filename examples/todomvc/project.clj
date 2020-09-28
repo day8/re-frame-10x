631009cb9a83eeb6776de945a46a5afb1344fec2
Isaac Johnston <isaac@superstruct.nz>
2020-09-28 15:21:28 +1300

    Upgrade lein-shadow to 0.3.1

--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -4,7 +4,7 @@
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs "2.9.10"]
+                 [thheller/shadow-cljs "2.11.4"]
                  [reagent "0.10.0"]
                  [re-frame "0.12.0"]
                  [day8.re-frame/tracing "0.5.6"]
@@ -12,7 +12,7 @@
                  [secretary "1.2.3"]]
 
   :plugins      [[day8/lein-git-inject "0.0.14"]
-                 [lein-shadow          "0.1.6"]]
+                 [lein-shadow          "0.3.1"]]
 
   :middleware   [leiningen.git-inject/middleware]
 
@@ -21,6 +21,8 @@
   :source-paths ["src" "../../src" "../../gen-src"]
 
   :clean-targets ^{:protect false} [:target-path
+                                    "shadow-cljs.edn"
+                                    "node_modules"
                                     "resources/public/js"]
 
   :shadow-cljs {:nrepl  {:port 8777}
@@ -36,4 +38,4 @@
                                :devtools   {:http-root "resources/public"
                                             :http-port 8280}}}}
 
-  :aliases {"dev-auto" ["with-profile" "dev" "shadow" "watch" "app"]})
+  :aliases {"watch" ["with-profile" "dev" "shadow" "watch" "app"]})
