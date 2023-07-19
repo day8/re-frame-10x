4ab5910837fb203e50a6ae42de2923f6504afe9f
Kimo Knowles <kimo.knowles@day8.com.au>
2023-07-19 12:51:48 +0200

    Extract highlighting calculations to a subscription
    `find-bounds` & its helpers aren't really view logic, IMO.
    They also seem like a reverse-engineering of our own libraries.
    
    I think we can delete them soon, so this is a step on the way.
    
    Next steps:
    
    - rewrite-clj records the string position of each form it parses
    - build a highlighter from rewrite-clj (in the following commit)
    - build re-frame.trace from rewrite-clj (I think it's doable)
    
    Once this is done, all our code will understand the position of forms,
    and we won't have to do any regex reverse-engineering.

--- src/day8/re_frame_10x/panels/event/subs.cljs
+++ src/day8/re_frame_10x/panels/event/subs.cljs
@@ -1,6 +1,8 @@
 (ns day8.re-frame-10x.panels.event.subs
   (:require
    [zprint.core                                                  :as zp]
+   [clojure.string                                               :as string]
+   [goog.string                                                  :as gstring]
    [day8.re-frame-10x.inlined-deps.re-frame.v1v1v2.re-frame.core :as rf]
    [day8.re-frame-10x.panels.settings.subs                              :as settings.subs]
    [day8.re-frame-10x.panels.traces.subs                                :as traces.subs]))
@@ -76,6 +78,85 @@
  :<- [::root]
  (fn [{:keys [highlighted-form]} _]
    highlighted-form))
+
+(defn- re-seq-idx
+  "Like re-seq but returns matches and indices"
+  ([re s] (re-seq-idx re s 0))
+  ([re s offset]  ;; copied from re-seq* impl https://github.com/clojure/clojurescript/blob/0efe8fede9e06b8e1aa2fcb3a1c70f66cad6392e/src/main/cljs/cljs/core.cljs#L10014
+   (when-some [matches (.exec re s)]
+     (let [match-str (aget matches 0)
+           match-vals (if (== (.-length matches) 1)
+                        match-str
+                        (vec matches))
+           match-index (.-index matches)]
+       (cons [match-vals, (+ offset match-index)]
+             (lazy-seq
+              (let [post-idx (+ (.-index matches)
+                                (max 1 (.-length match-str)))]
+                (when (<= post-idx (.-length s))
+                  (re-seq-idx re (subs s post-idx) (+ offset post-idx))))))))))
+
+(defn collapse-whitespace-and-index
+  "given a string argument `s` it will return a vector of two values:
+     - a modified version of `s`, call it s'
+     - a vector of indexes, v
+   s' will be a copy of s in which all consecutive whitespace is collapsed to one whitespace
+   v  will be a vector of index for characters in s' back to the original s
+   For example:
+      (collapse-whitespace-and-index \"a b  c\")
+   will return
+       [\"a b c\" [0 1 2 3 5]]     ;; notice that the 4 is not there
+   "
+  [s]
+  (let [s' (clojure.string/replace s #"\s+" " ") ;; generate a new string with whitespace replaced
+        v (loop [v []     ;; Build up an index between the string with and without whitespace
+                 i-s 0
+                 i-s' 0]
+            (cond
+              (= (count s') i-s')           (conj v (count s)) ;; we have reached the end of both strings
+              (= (nth s i-s) (nth s' i-s')) (recur (conj v i-s) (inc i-s) (inc i-s')) ;; when we have a match save the index
+              :else                         (recur v (inc i-s) i-s')))]    ;; no match (whitespace) increment the index on the orignal string
+    [s' v]))
+
+(defn find-bounds
+  "Try and find the bounds of the form we are searching for. Uses some heuristics to
+  try and avoid matching partial forms, e.g. 'default-|weeks| for the form 'weeks."
+  [form-str search-str num-seen]
+  (if (nil? search-str)
+    [0 0]  ;; on mouse out etc
+    (let [[form-str reindex]   (collapse-whitespace-and-index form-str) ;; match without whitespace
+          esc-str    (gstring/regExpEscape search-str)
+          regex      (str "(\\s|\\(|\\[|\\{)" "(" esc-str ")(\\s|\\)|\\]|\\})")
+          re         (re-pattern regex)
+          results    (re-seq-idx re form-str)]
+      ;; (js/console.log "FIND-BOUNDS" form-str  regex reindex results)
+      (if (and search-str num-seen (seq results) (>= (count results)  num-seen))
+        (let [result                              (nth results (dec num-seen))
+              [[_ pre-match matched-form] index]  result
+              index                               (+ index (count pre-match))
+              start                               (nth reindex index)
+              stop                                (nth reindex (+ index (count matched-form)))]
+          [start stop])
+        ;; If the regex fails, fall back to string index just in case.
+        (let [start  (some->> form-str
+                              (string/index-of (pr-str search-str))
+                              (nth reindex))
+              length (if (some? start)
+                       (count (pr-str search-str))
+                       1)
+              end    (some->> start
+                              (+ length)
+                              (nth reindex))]
+          [start end])))))
+
+(rf/reg-sub
+ ::highlighted-form-bounds
+ :<- [::highlighted-form]
+ :<- [::form-for-epoch]
+ (fn [[highlighted-form form] _]
+   (find-bounds (str form)
+                (:form highlighted-form)
+                (:num-seen highlighted-form))))
 
 (rf/reg-sub
  ::show-all-code?
